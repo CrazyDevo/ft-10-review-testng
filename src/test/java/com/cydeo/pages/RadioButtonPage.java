489bc8e2bdf6d03758e303e90584823f6f51e8b7
AdamB25 <barsialtunit@gmail.com>
2025-03-22 12:13:05 -0400

    radio button example

--- src/test/java/com/cydeo/pages/RadioButtonPage.java
+++ src/test/java/com/cydeo/pages/RadioButtonPage.java
@@ -1,6 +1,32 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class RadioButtonPage {
 
+    public RadioButtonPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "blue")
+    public WebElement blueRadioButton;
+
+    @FindBy(id = "football")
+    public WebElement footballRadioButton;
+
+public void  navigate(){
+    Driver.getDriver().get(ConfigurationReader.getProperty("practice.url")+"/radio_buttons");
+}
+
+
+/*
+    static {
+        Driver.getDriver().get(ConfigurationReader.getProperty("practice.url")+"/radio_buttons");
+    }
+*/
 
 }
--- src/test/java/com/cydeo/tests/Se01_RadioButtonTests.java
+++ src/test/java/com/cydeo/tests/Se01_RadioButtonTests.java
@@ -1,5 +1,9 @@
 package com.cydeo.tests;
 
+import com.cydeo.pages.RadioButtonPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Se01_RadioButtonTests {
@@ -21,17 +25,28 @@ public class Se01_RadioButtonTests {
     @Test
     public void test1() {
         //1.GO TO: http://practice.cydeo.com/radio_buttons
+        RadioButtonPage radioButtonPage=new RadioButtonPage();
+        radioButtonPage.navigate();
+
 
         //2. Confirm radio button #Blue is selected by default
+        boolean selected = radioButtonPage.blueRadioButton.isSelected();
+        Assert.assertTrue(selected);
+
         //3. Confirm radio button #Football is NOT selected by default
+        Assert.assertFalse(radioButtonPage.footballRadioButton.isSelected());
         //4. Click radio button #Football to select it.
+        radioButtonPage.footballRadioButton.click();
         //5. Verify radio button #Football is selected.
+        Assert.assertTrue(radioButtonPage.footballRadioButton.isSelected());
         //6. Click radio button #Red to select it.
         //7. Verify radio button #Red is selected.
         //8. Verify radio button #Blue is not selected.
         //9. Verify radio button #Green is not Enabled.
 
 
+        BrowserUtils.sleep(3);
+        Driver.closeDriver();
 
     }
 }
