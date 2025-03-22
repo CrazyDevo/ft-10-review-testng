11007c13becb1dee4b7c6f145385c4d266ff3c7d
AdamB25 <barsialtunit@gmail.com>
2025-03-22 11:46:08 -0400

    utilities package is ready

---
+++ src/test/java/com/cydeo/tests/Se01_RadioButtonTests.java
@@ -0,0 +1,37 @@
+package com.cydeo.tests;
+
+import org.testng.annotations.Test;
+
+public class Se01_RadioButtonTests {
+
+
+
+    //TC #1:radio buttons
+    //1.GO TO: http://practice.cydeo.com/radio_buttons
+    //2. Confirm radio button #Blue is selected by default
+    //3. Confirm radio button #Football is NOT selected by default
+    //4. Click radio button #Football to select it.
+    //5. Verify radio button #Football is selected.
+    //6. Click radio button #Red to select it.
+    //7. Verify radio button #Red is selected.
+    //8. Verify radio button #Blue is not selected.
+    //9. Verify radio button #Green is not Enabled.
+
+
+    @Test
+    public void test1() {
+        //1.GO TO: http://practice.cydeo.com/radio_buttons
+
+        //2. Confirm radio button #Blue is selected by default
+        //3. Confirm radio button #Football is NOT selected by default
+        //4. Click radio button #Football to select it.
+        //5. Verify radio button #Football is selected.
+        //6. Click radio button #Red to select it.
+        //7. Verify radio button #Red is selected.
+        //8. Verify radio button #Blue is not selected.
+        //9. Verify radio button #Green is not Enabled.
+
+
+
+    }
+}
